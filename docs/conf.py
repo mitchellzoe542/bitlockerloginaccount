bf75bfd345f067ae32cf6ad4bc14a9a8d137cc3d
mitchellzoe542 <mitchellzoe542@gmail.com>
2025-06-19 15:02:35 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -9,8 +9,8 @@ exclude_patterns = []
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-# Ensure static path is defined
+# This tells Sphinx where to find static files like images
 html_static_path = ['_static']
 
-# Add this line to include favicon
-html_favicon = '_static/favicon.png'
+# This tells Sphinx which favicon to use
+html_favicon = '_static/favicon.ico'  # or favicon.png
